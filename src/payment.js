a720885a22b3ddf7ca5f4002fa78df9c6a1d809b
Ayumi Saito <ayumisaito@Ayumis-MacBook-Air.local>
2017-09-20 16:39:37 +0200

    edit styling table

--- src/payment.js
+++ src/payment.js
@@ -30,7 +30,7 @@ let headerProps = {
 
 // initial set of rows, simulating data from the database
 let rows = [
-  {date: "12:30 12.9.2017", payment: "MasterCard", narrative: "restige Cosmetics, Total Intensity Eyeliner Long Lasting Intense Color, Deepest Black, 1.2 g (.04 oz)", amount: "$912.51", uniqueId: 0 },
+  {date: "12:30 12.9.2017", payment: "MasterCard", narrative: "restige Cosmetics, Total Intensity Eyeliner Long Lasting Intense Color, Deepest Black, 1.2 g (.04 oz)", amount: "$912.51" , uniqueId: 0 },
   {date: "11:30 12.9.2017", payment: "Visa", narrative: "Total Intensity Eyeliner Long Lasting Intense Color, Deepest Black, 1.2 g (.04 oz)", amount: "$744.51", uniqueId: 1 },
   {date: "13:30 12.9.2017", payment: "PayPal", narrative: "Eyeliner Long Lasting Intense Color, Deepest Black, 1.2 g (.04 oz)", amount: "$12.51", uniqueId: 2 },
   {date: "20:30 12.9.2017", payment: "MasterCard", narrative: "Long Lasting Intense Color, Deepest Black, 1.2 g (.04 oz)", amount: "$16.51", uniqueId: 3 }
@@ -54,7 +54,8 @@ let headers = [
   {name: "Today", key: "date"},
   {name: "Payment", key: "payment"},
   {name: "Narrative", key: "narrative"},
-   {name: "Amount", key: "amount"}
+  {name: "Amount", key: "amount"},
+  {name :"" , key : "dotMenu"}
 ];
 
 // our table component that can sort columns
@@ -64,16 +65,18 @@ class SortableTable extends React.Component {
     super(props);
     this.state = {rows, 
                   sortBy: 'firstName',
-                  tableOpen : false
+                  tableOpen : false,
+                  open: false,
                 };
   }
-
-   handleClick = (event) => {
+  
+  handleClick = (event) => {
     event.preventDefault();
     this.setState({
       tableOpen : !this.state.tableOpen
     })
   }
+  
 
   renderHeaders(){
     let header= headers.map( (h) => {
@@ -149,19 +152,64 @@ function SortableHeader(props){
 }
   
 
-function UserRow(props){
+class UserRow extends React.Component {
+
+   constructor(props){
+    super(props);
+    this.state = {
+                  open: false,
+                };
+  }
+  
+   handleTouchTap = (event) => {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+    console.log(this.state.open)
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  render(){
   return (
-    <TableRow>
-      <TableRowColumn><img src={Check} alt="Check" className="Check"/></TableRowColumn>
-      <TableRowColumn>{props.date}</TableRowColumn>
-      <TableRowColumn>{props.payment=="Visa" ?  <img src={Visa} alt="Visa" className="Visa"/> :  (props.payment=="PayPal" ?  <img src={Paypal} alt="Paypal" className="Master"/> :  <img src={Master} alt="Master" className="Master"/>)}
+   
+    <TableRow className="tableRow">
+      <TableRowColumn className="tableOne">{this.props.payment=="PayPal"?  <img src={Fail} alt="Check" className="Fail"/> : <img src={Check} alt="Check" className="Check"/> }</TableRowColumn>
+      <TableRowColumn className="tableTwo">{this.props.date}</TableRowColumn>
+      <TableRowColumn className="tableThree">{this.props.payment=="Visa" ?  <img src={Visa} alt="Visa" className="Visa"/> :  (this.props.payment=="PayPal" ?  <img src={Paypal} alt="Paypal" className="Master"/> :  <img src={Master} alt="Master" className="Master"/>)}          
+         &nbsp;  &nbsp;  &nbsp;  {this.props.payment}</TableRowColumn>
+      <TableRowColumn className="tableFour">{this.props.narrative}</TableRowColumn>
+      <TableRowColumn className="tableFive">{this.props.amount}</TableRowColumn>
+      <TableRowColumn className="tableSix"><img src={Dotmenu} alt="Dotmenu" className="Dotmenu" onClick={this.handleTouchTap} /> </TableRowColumn>
 
+         <Popover
+          zDepth={5}
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          onRequestClose={this.handleRequestClose}
+        >
+        
+         <Menu>
+            <MenuItem primaryText="Item1" />
+            <MenuItem primaryText="Item2" />
+            <MenuItem primaryText="Item3" />
+            <MenuItem primaryText="Item4" />
+          </Menu>
+        </Popover>
 
-     {props.payment}</TableRowColumn>
-      <TableRowColumn>{props.narrative}</TableRowColumn>
-      <TableRowColumn>{props.amount}</TableRowColumn>
     </TableRow>
   );
+}
 }
 
 
